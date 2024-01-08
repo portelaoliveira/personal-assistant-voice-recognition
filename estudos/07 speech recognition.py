f1951f88315673ac5f427bbd6cf3a75684f77cc4
Danilo Portela <daniloportela97@gmail.com>
2024-01-08 20:33:36 -0300

    first commit

---
+++ estudos/07 speech recognition.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+
+
+def cria_audio(audio, mensagem):
+    tts = gTTS(mensagem, lang="pt-br")
+    tts.save(audio)
+    playsound(audio)
+
+
+cria_audio("welcome.mp3", "Olá, sou o Portela! Vou reconhecer a sua voz.")
+
+recon = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Diga algo:")
+    audio = recon.listen(source)
+
+frase = recon.recognize_google(audio, language="pt")
+cria_audio("mensagem.mp3", frase)
